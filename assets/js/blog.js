fb4af3baf2f5dfc1b4b75f10910e0b5297e972de
mjurek1 <jurekmichael0@gmail.com>
2024-05-14 23:23:50 -0500

    updated blog

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,1 +1,29 @@
 // Grab all the references to the DOM elements
+const blogPost = document.querySelector('#blog-posts');
+
+const posts = [];
+
+function renderPosts() {
+    blogPost.innerHTML = '';
+
+    for (let i = 0; i < posts.length; i++) {
+        const post = posts[i];
+
+        const h1 = document.createElement('h1');
+        h1.textContent = post
+        
+    }
+}
+
+function init() {
+    const storedPosts = JSON.parse(localStorage.getItem('posts'));
+
+    if(storedPosts !== null) {
+        posts = storedPosts;
+    }
+    renderPosts();
+}
+
+function storedPosts () {
+    localStorage.setItem('posts', JSON.stringify(posts));
+}
--- assets/js/form.js
+++ assets/js/form.js
@@ -6,6 +6,11 @@ const submitButton = document.getElementById('submit');
 submitButton.addEventListener('click', function (event) {
     event.preventDefault();
 
+    if(!userName.value || !title.value || !content.value) {
+        alert("please complete the form to submit a blog post");
+        return;
+    }
+
     const blogEntry = {
         userName: userName.value,
         title: title.value,
@@ -13,5 +18,6 @@ submitButton.addEventListener('click', function (event) {
     };
 
     localStorage.setItem('blogEntry', JSON.stringify(blogEntry));
+    document.location = "blog.html";
 });
 
